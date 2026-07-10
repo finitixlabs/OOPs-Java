1ee695b49b18ecab2030e7dd1f6b07bdfdd84fae
Jnaneswar <jnaneswar.official@gmail.com>
2026-07-10 21:38:52 +0530

    Constructor Over riding and Method overloading and this and super keyword

--- Constructor/Basics.java
+++ Constructor/Basics.java
@@ -1,4 +1,24 @@
 package Constructor;
+class A{
+    public A(){// Method Overloading Because same method name but different parameters
+        System.out.println("in A");
+    }
+    public A(int n){// Method Overloading Because same method name but different parameters
+        System.out.println("In Int A");
+    }
+}
+class B extends A{
+    public B(){
+        System.out.println("in B");
+    }
 
+    public B(int n){
+        this();
+        System.out.println("In Int B");
+    }
+}
 public class Basics {
+    public static void main(String[] args){
+        B obj = new B(10);
+    }
 }
